cfdd90bc7a2b68d949b479feb10761330f99c4b3
Rahul Singh Takuli <rahulsinghkb56@gmail.com>
2023-12-06 00:03:44 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import NavItems from "./components/NavItems";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>hy</h1>
+      <NavItems />
+      <div className="min-vh-100">
+      <Outlet />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
